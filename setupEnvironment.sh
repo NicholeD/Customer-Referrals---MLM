b614a4156d767c32679d24cea5184bda9f1ba34f
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-11-09 15:53:10 -0700

    updated setupEnvironment.sh file with name in file path.

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -9,7 +9,7 @@
 
 # Fill out the following values
 # The path of your repo on github.  Don't but the whole URL, just the part after github.com/
-export UNIT_FIVE_REPO_NAME=ata-unit-five-project-$GITHUB_USERNAME
+export UNIT_FIVE_REPO_NAME=ata-unit-five-project-NicholeD
 
 # Do not modify the rest of these unless you have been instructed to do so.
 export UNIT_FIVE_PROJECT_NAME=unitproject5
